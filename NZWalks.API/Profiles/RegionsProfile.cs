a78c769ebf2f94ae9909bb2cc589aa219e24981f
Sreedhar2777 <psreedhar32@gmail.com>
2024-01-05 18:03:22 +0530

    Added DTO'S , Auto Mapper and Async Operations to the Regions

---
+++ NZWalks.API/Profiles/RegionsProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace NZWalks.API.Profiles
+{
+    public class RegionsProfile:Profile
+    {
+        public RegionsProfile()
+        {
+                CreateMap<Model.Domain.Region, Model.DTO.Region>();
+                
+        }
+    }
+}
